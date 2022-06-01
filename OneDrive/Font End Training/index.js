c98b21513d0468f2fb7e9d74b38953f80fd9bbe8
Kali Campbell <ilak44315@gmail.com>
2022-05-31 20:50:26 -0500

    commit assignment

---
+++ OneDrive/Font End Training/index.js
@@ -0,0 +1,9 @@
+var costOfMilk = 2;
+
+if (costOfMilk <2){
+    console.log('We will buy 2 gallons');
+} else if (costOfMilk < 3){ 
+    console.log('We will buy only 1 gallon');
+}
+    else { console.log ('No thanks, way too expensive');
+}
